8829dbe80056e70897031c2e52918ff6053fb955
JAYDEN <jaydenleondsouza@gmail.com>
2026-01-27 23:11:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// this line lets Express serve index.html from /public
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("message", (msg) => {
+    io.emit("message", msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Chat running on port 3000");
+});
